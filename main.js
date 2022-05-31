1f10f4f7922ddadb8974130a3578d96897100924
ismaortiz <isma297396@gmail.com>
2022-05-30 22:11:46 -0300

    first2last

--- main.js
+++ main.js
@@ -24,6 +24,19 @@ function main() {
     menu.classList.toggle("header__nav__menu--show");
     body.classList.toggle("stuck");
   });
+  const form = document.querySelector(".contact__form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const to = "isma297396@gmail.com";
+    const name = e.target["name"].value;
+    const from = e.target["mail"].value;
+    const message = e.target["message"].value;
+    const request = fetch(`https://apx-api.vercel.app/api/utils/dwf`, {
+      method: "POST",
+      body: JSON.stringify({ name, from, to, message }),
+      headers: { "content-type": "application/json" },
+    }).then((response) => console.log(response));
+  });
   fetch(
     "https://cdn.contentful.com/spaces/ckd7g74pb4a1/environments/master/entries?access_token=1pG9l9Oou2_WGwenhIUUNn3mF2su2ElYYTXuwu-2dnY&content_type=services"
   )
