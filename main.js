f82528aec7cce527533f0f2c786f531c83ebe1e9
ismaortiz <isma297396@gmail.com>
2022-05-30 22:05:26 -0300

    firstndlast

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ let menu = document.querySelector(".header__nav__menu");
 let body = document.querySelector("body");
 const boton = document.querySelector(".header__nav__hambut");
 
+let subtitles = document.querySelectorAll(".myservices__subtitles");
+// let descriptions = document.querySelectorAll(".myservices__par");
+let imgs = document.querySelectorAll(".myservices__icons");
+
+function loadPro(data) {
+  const textData = data["items"];
+  const mediaData = data["includes"]["Asset"];
+  for (let i = 0; i < subtitles.length; i++) {
+    subtitles[i].textContent = textData[i]["fields"]["projectTitle"];
+    // descriptions[i].textContent = textData[i]["fields"]["projectDescription"];
+    imgs[i].src = mediaData[i]["fields"]["file"]["url"];
+  }
+}
+
 function main() {
   boton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -10,27 +24,10 @@ function main() {
     menu.classList.toggle("header__nav__menu--show");
     body.classList.toggle("stuck");
   });
+  fetch(
+    "https://cdn.contentful.com/spaces/ckd7g74pb4a1/environments/master/entries?access_token=1pG9l9Oou2_WGwenhIUUNn3mF2su2ElYYTXuwu-2dnY&content_type=services"
+  )
+    .then((data) => data.json())
+    .then((data) => loadPro(data));
 }
 main();
-
-// let titles = document.querySelectorAll(".pro-item-title");
-// let descriptions = document.querySelectorAll(".pro-item-description");
-// let imgs = document.querySelectorAll(".pro-item-img");
-
-// function loadPro(data) {
-//   const textData = data["items"];
-//   const mediaData = data["includes"]["Asset"];
-//   for (let i = 0; i < titles.length; i++) {
-//     titles[i].textContent = textData[i]["fields"]["projectTitle"];
-//     descriptions[i].textContent = textData[i]["fields"]["projectDescription"];
-//     imgs[i].src = mediaData[i]["fields"]["file"]["url"];
-//   }
-// }
-// async function main() {
-//   await fetch(
-//     "https://cdn.contentful.com/spaces/ckd7g74pb4a1/environments/master/entries?access_token=1pG9l9Oou2_WGwenhIUUNn3mF2su2ElYYTXuwu-2dnY"
-//   )
-//     .then((data) => data.json())
-//     .then((data) => loadPro(data));
-// }
-// main();
